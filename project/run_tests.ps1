dde3eaf740e916ad043bcaf9f580f49baf65fab6
Samuel Nicholas <nicholas.samuel@gmail.com>
2026-02-22 17:14:14 +1030

    stripped to just the addon, and moved files to root.

--- project/run_tests.ps1
+++
@@ -1,4 +0,0 @@
-
-$project_path = 'C:\Godot\extensions\gdflatbuffers\project'
-$godot_exe = 'C:\build\godot\w64.scons.msvc.x86_64.windows.editor.dev_build.4.5\bin\godot.windows.editor.dev.x86_64.exe'
-& $godot_exe --path $project_path --headless -s run_tests_cli.gd --quit
